48a440f63f5571e70f267f9032b12702c07ff031
sebamora84 <sebamora@gmail.com>
2017-03-14 22:48:51 -0300

    change hall load (not working)

--- Scripts/hall.js
+++ Scripts/hall.js
@@ -1,4 +1,8 @@
-var tables
+var tables;
+var selectedTable;
+var consumptionTypes;
+var currentConsumption;
+
 $(function(){
 	$(document).keydown(function(e) {
         if (e.keyCode == 66 && e.ctrlKey) {
@@ -7,16 +11,10 @@ $(function(){
     });
 		
 	loadAllCredits();
+	loadAllConsumptionTypes();
+	loadAllTables();
 	
-	$.post("Api/Consumption/GetAllConsumptionTypes.php",
-		function(data, status){
-			loadConsumptionTypes(data, status);
-	});
-	$.post("Api/Table/GetAllTables.php",
-		function(data, status){
-			loadTables(data, status);
-			loadOpenConsumptionTables();
-	});
+	
 	
 	$.post("Api/Product/GetAllProducts.php",
 			function(data, status){
@@ -135,173 +133,197 @@ function addItemByProduct(product, table){
 				selectTable(table);
 		});
 }
-function loadConsumptionTypes(data, status){
-	var jsonData = JSON.parse(data);
-	if (jsonData.length == 0) { return; };
-	$( ".consumptionTypeOptions" ).empty();
-	$.each(jsonData, function(index, element) {
-		//Create all the divs					
-		consumptionType = element;
-		$( ".consumptionTypeOptions" ).append( '<div id="consumptionType' + consumptionType.id + '" class="consumptionType">' + consumptionType.description+ '</div>' )
+function loadAllCredits(){
+	$.post("Api/Consumption/GetAllCredits.php",
+			function(data, status){
+		$("#creditSelector").empty();
+		$('#creditSelector').append('<option value=""> Seleccione una cuenta ...</option>');
+		credits = JSON.parse(data);
+		if (credits.length == 0) { return; };
+		
+		$.each(credits, function(index, credit){					
+			$('#creditSelector').append('<option value='+credit.id+'>'+credit.description+'</option>');
+		});
 	});
 }
-function loadTables(data, status){
-	tables = JSON.parse(data);
-	if (tables.length == 0) { return; };
-	$.each(tables, function(index, element) {
-		//Create all the divs and set position
-		table = element;
-		$( "#hall" ).append( '<div id="table'+ table.id +'" class="table"> '+table.description+' </div>' );	
-		$('#table'+ table.id)
-			.css({
-			   left: table.left, 
-			   top: table.top,
-			   width: table.width, 
-			   height: table.height});		
-	});
-	//When drag or resize save position
-	$( ".table" ).draggable({
-		grid: [ 5, 5 ],
-		scroll: true,
-		stop: function(event) {
-			target = event.target;
-			tableId = target.id.replace('table','');
-			tableLeft = $(target).css('left');
-			tableTop = $(target).css('top');
-			tableWidth = $(target).css('width');
-			tableHeight = $(target).css('height');
-			$.post("Api/Table/MoveTable.php",
-				{ id: tableId, left: tableLeft, top:tableTop, width:tableWidth, height:tableHeight  },
-				function(data, status){
-					//message saved
-				});	
-		  }
-	}).resizable({
-		stop: function(event) {
-			target = event.target;
-			tableId = target.id.replace('table','');
-			tableLeft = $(target).css('left');
-			tableTop = $(target).css('top');
-			tableWidth = $(target).css('width');
-			tableHeight = $(target).css('height');
-			$.post("Api/Table/MoveTable.php",
-				{ id: tableId, left: tableLeft, top:tableTop, width:tableWidth, height:tableHeight  },
-				function(data, status){
-					//message saved
-				});	
-		  }
-	}).droppable({
-        drop: function( event, ui ) {
-			product = ui.draggable[0];
-			table = $(this).closest('.table');
-			addItemByProduct(product, table);
-        }
-	});
-	//Make cafe tables selectable
-	$( ".table" ).click(function(event) {
-		table = $(this).closest('.table');
-		selectTable(table);
+function loadAllConsumptionTypes(){
+	$.post("Api/Consumption/GetAllConsumptionTypes.php",
+			function(data, status){
+			consumptionTypes = JSON.parse(data);
+			if (consumptionTypes.length == 0) { return; };
+			$( ".consumptionTypeOptions" ).empty();
+			$.each(consumptionTypes, function(index, consumptionType) {
+				//Create all the divs	
+				$( ".consumptionTypeOptions" ).append( '<div id="consumptionType' + consumptionType.id + '" class="consumptionType">' + consumptionType.description+ '</div>' )
+			});
+		});	
+}
+function loadAllTables(){
+	$.post("Api/Table/GetAllTables.php",
+		function(data, status){
+		tables = JSON.parse(data);
+		if (tables.length == 0) { 
+			//TODO: log error
+			return; 
+		};
+		$.each(tables, function(index, table) {
+			//Create all the divs and set position
+			$( "#hall" ).append( '<div id="table'+ table.id +'" class="table"> <label> '+table.description+' <label></div>' );	
+			$('#table'+ table.id)
+				.css({
+				   left: table.left, 
+				   top: table.top,
+				   width: table.width, 
+				   height: table.height});		
+		});
+		//When drag or resize save position
+		$( ".table" ).draggable({
+			grid: [ 5, 5 ],
+			scroll: true,
+			stop: function(event) {
+				target = event.target;
+				tableId = target.id.replace('table','');
+				tableLeft = $(target).css('left');
+				tableTop = $(target).css('top');
+				tableWidth = $(target).css('width');
+				tableHeight = $(target).css('height');
+				$.post("Api/Table/MoveTable.php",
+					{ id: tableId, left: tableLeft, top:tableTop, width:tableWidth, height:tableHeight  },
+					function(data, status){
+						//message saved
+					});	
+			  }
+		}).resizable({
+			stop: function(event) {
+				target = event.target;
+				tableId = target.id.replace('table','');
+				tableLeft = $(target).css('left');
+				tableTop = $(target).css('top');
+				tableWidth = $(target).css('width');
+				tableHeight = $(target).css('height');
+				$.post("Api/Table/MoveTable.php",
+					{ id: tableId, left: tableLeft, top:tableTop, width:tableWidth, height:tableHeight  },
+					function(data, status){
+						//message saved
+					});	
+			  }
+		}).droppable({
+	        drop: function( event, ui ) {
+				product = ui.draggable[0];
+				table = $(this).closest('.table');
+				addItemByProduct(product, table);
+	        }
+		});
+		//Make cafe tables selectable
+		$( ".table" ).click(function(event) {
+			tableId = $(this).closest('.table').attr('id').replace('table','');
+			selectTable(tableId);
+		});
+		loadOpenConsumptionTables();
+		loadConsumption();
 	});
 }
-function selectTable(table){
+function selectTable(tableId){
 	$(".table").removeClass("tableSelected");
-	$(table).addClass("tableSelected");
-	tableId = $(table).attr('id').replace('table','');
-	loadConsumption(tableId);
-	loadOpenConsumptionTables();
+	selectedTable=tables[tableId];
+	if(selectedTable==null){
+		return;
+	}
+	$('#table'+selectedTable.id).addClass("tableSelected");
+	loadConsumption();
 }
-function loadConsumption(tableId){
+function loadConsumption(){
 $("#consumptionOpened").hide();
 $("#consumptionClosed").hide();
-$('.consumptionInfo').empty()
-$('#consumption ul').empty()
-//Load Table data
-$.post("Api/Table/GetTable.php",
-		{ id: tableId},
-		function(data, status){
-		table = JSON.parse(data);					
-		$( ".consumptionInfo" ).attr('id', 'consumptionInfo0');
-		$( ".consumptionInfo" ).append( '<p> '+table.description+' </p>' );
+
+$('#consumptionInfo').empty();
+var consumptionHeader = $('#consumptionHeaderTemplate').clone();
+$(consumptionHeader).attr('id', 'consumptionHeader');
+$("#consumptionInfo").append(consumptionHeader);
+
+if (selectedTable==null){
+	return;
+}
+$('#consumptionHeader .tableName').text(selectedTable.description);
+
+//Load Consumption data
+$.post("Api/Consumption/GetCurrentConsumption.php",
+	{ id: selectedTable.id},
+	function(data, status){
+		currentConsumption = JSON.parse(data);
+		if (currentConsumption==null) {
+			$("#consumptionHeader .type").text(consumptionTypes[selectedTable.default_consumption_type_id].description);
+			$("#consumptionClosed").show();
+			return; 
+		};
+		$("#consumptionHeader .type").text(currentConsumption.consumption_type_description);
+		$("#consumptionHeader .total").text('Total: $'+parseFloat(Math.round(currentConsumption.total * 100) / 100).toFixed(2));
+		$("#consumptionOpened").show();
 		
 		//Load Consumption data
-		$.post("Api/Consumption/GetCurrentConsumption.php",
-			{ id: tableId},
+		$.post("Api/Consumption/GetItemsByConsumption.php",
+			{ id: currentConsumption.id},
 			function(data, status){
-				consumption = JSON.parse(data);
-				if (consumption==null) {
+				items = JSON.parse(data);
+				if (items.length == 0) { return; };
+				$.each(items, function(index, item) {
+					//Create all the divs and
+
+					var consumptionItem = $('#consumptionItemTemplate').clone();
+					$(consumptionItem).attr('id', 'consumptionItem'+item.id);
+					$("#consumptionInfo").append(consumptionItem);
 					
-					consumptionType = $("#consumptionType"+table.default_consumption_type_id).closest('.consumptionType');
-					$(".consumptionType").removeClass("consumptionTypeSelected");
-					$(consumptionType).addClass("consumptionTypeSelected");
-					$( ".consumptionInfo" ).append( '<p>'+ consumptionType[0].innerHTML +' </p>' );
-					$("#consumptionClosed").show();
-					return; 
-				};				
-				$("#consumptionOpened").show();
-				$( ".consumptionInfo" ).attr('id', 'consumptionInfo' + consumption.id);				
-				$( ".consumptionInfo" ).append( '<p>'+consumption.consumption_type_description+' </p>' );
-				$( ".consumptionInfo" ).append( '<p>Total: $'+parseFloat(Math.round(consumption.total * 100) / 100).toFixed(2)+' </p>' );
-				//Load Consumption data
-				$.post("Api/Consumption/GetItemsByConsumption.php",
-					{ id: consumption.id},
-					function(data, status){
-						var jsonData = JSON.parse(data);
-						if (jsonData.length == 0) { return; };
-						consumptionTotal = 0.00;
-						$.each(jsonData, function(index, element) {
-							//Create all the divs and 
-							item = element;
-							$( "#consumption ul" ).append( '<li> <div class="item" id="item'+item.id+'" > <div class="itemPartLeft" >'+ item.quantity +' </div> <div class="itemPartLeft">'+item.product_description+' </div> <div class="itemPartRight"><img id="deleteItem'+item.id+'" class="itemButton buttonDeleteItem" src="Images/Icons/remove.png" alt="-" height="16"></div><div class="itemPartRight"><img id="addItem'+item.id+'" class="itemButton buttonAddItem" src="Images/Icons/add.png" alt="+" height="16"></div><div class="itemPartRight"> $ '+parseFloat(Math.round(item.subtotal * 100) / 100).toFixed(2)+'</div></div></li> ' );
-							consumptionTotal += parseFloat(item.subtotal);
-						});
-						
-					$( "#consumption ul" ).append( '<li><div class="item itemSubtotal" ><div class="itemPartLeft" >Subtotal:</div><div class="itemPartRight" > $ '+parseFloat(Math.round(consumption.subtotal * 100) / 100).toFixed(2)+'</div> </div> </li> ' );
-					$( "#consumption ul" ).append( '<li><div class="item itemDiscount" ><div class="itemPartLeft" >'+consumption.discount_description+':</div><div class="itemPartRight" ><input id="consumptionDiscount"  value="'+parseFloat(Math.round(consumption.discount * 100) / 100).toFixed(2)+'"></div><div class="itemPartRight" > - $  </div></div> </li> ' );
-					$( "#consumption ul" ).append( '<li><div class="item itemTotal" ><div class="itemPartLeft" >Total:</div><div class="itemPartRight" > $ '+parseFloat(Math.round(consumption.total * 100) / 100).toFixed(2)+' </div> </div> </li> ' );
+					//$( "#consumption ul" ).append( '<li> <div class="item" id="item'+item.id+'" > <div class="itemPartLeft" >'+ item.quantity +' </div> <div class="itemPartLeft">'+item.product_description+' </div> <div class="itemPartRight"><img id="deleteItem'+item.id+'" class="itemButton buttonDeleteItem" src="Images/Icons/remove.png" alt="-" height="16"></div><div class="itemPartRight"><img id="addItem'+item.id+'" class="itemButton buttonAddItem" src="Images/Icons/add.png" alt="+" height="16"></div><div class="itemPartRight"> $ '+parseFloat(Math.round(item.subtotal * 100) / 100).toFixed(2)+'</div></div></li> ' );
 					
-					$( ".buttonDeleteItem" ).click(function(event) {
-						button = $(this).closest('.buttonDeleteItem');
-						itemId = $(button).attr('id').replace('deleteItem','');
-						$.post("Api/Consumption/SubstractItem.php",
-								{ itemId: itemId},
-								function(data, status){
-									table =$('.tableSelected');
-									selectTable(table);
-							});									
-					});
-					$( ".buttonAddItem" ).click(function(event) {
-						button = $(this).closest('.buttonAddItem');
-						itemId = $(button).attr('id').replace('addItem','');
-						$.post("Api/Consumption/AddItem.php",
-								{ itemId: itemId},
-								function(data, status){
-									table =$('.tableSelected');
-									selectTable(table);
-							});									
-					});
-					$("#consumptionDiscount").focus(function(event) {
-						this.select();
-					}).focusout(function(event) {
-						discount= $(this).val()
-						//TODO: edit discount description and add it to the post
-						discountDescription="Descuento";
-						consumptionId =  $(".consumptionInfo").attr('id').replace('consumptionInfo','');
-						$.post("Api/Consumption/ApplyConsumptionDiscount.php",
-								{ consumptionId: consumptionId, discountDescription:discountDescription ,discount: discount},
-								function(data, status){
-									table =$('.tableSelected');
-									selectTable(table);
-							});
-					}).keyup(function(e){
-					    if(e.keyCode == 13)
-					    {
-					        $(this).trigger("focusout");
-					    }
-					});
 				});
 				
+			$( "#consumption ul" ).append( '<li><div class="item itemSubtotal" ><div class="itemPartLeft" >Subtotal:</div><div class="itemPartRight" > $ '+parseFloat(Math.round(consumption.subtotal * 100) / 100).toFixed(2)+'</div> </div> </li> ' );
+			$( "#consumption ul" ).append( '<li><div class="item itemDiscount" ><div class="itemPartLeft" >'+consumption.discount_description+':</div><div class="itemPartRight" ><input id="consumptionDiscount"  value="'+parseFloat(Math.round(consumption.discount * 100) / 100).toFixed(2)+'"></div><div class="itemPartRight" > - $  </div></div> </li> ' );
+			$( "#consumption ul" ).append( '<li><div class="item itemTotal" ><div class="itemPartLeft" >Total:</div><div class="itemPartRight" > $ '+parseFloat(Math.round(consumption.total * 100) / 100).toFixed(2)+' </div> </div> </li> ' );
+			
+			$( ".buttonDeleteItem" ).click(function(event) {
+				button = $(this).closest('.buttonDeleteItem');
+				itemId = $(button).attr('id').replace('deleteItem','');
+				$.post("Api/Consumption/SubstractItem.php",
+						{ itemId: itemId},
+						function(data, status){
+							table =$('.tableSelected');
+							selectTable(table);
+					});									
+			});
+			$( ".buttonAddItem" ).click(function(event) {
+				button = $(this).closest('.buttonAddItem');
+				itemId = $(button).attr('id').replace('addItem','');
+				$.post("Api/Consumption/AddItem.php",
+						{ itemId: itemId},
+						function(data, status){
+							table =$('.tableSelected');
+							selectTable(table);
+					});									
+			});
+			$("#consumptionDiscount").focus(function(event) {
+				this.select();
+			}).focusout(function(event) {
+				discount= $(this).val()
+				//TODO: edit discount description and add it to the post
+				discountDescription="Descuento";
+				consumptionId =  $(".consumptionInfo").attr('id').replace('consumptionInfo','');
+				$.post("Api/Consumption/ApplyConsumptionDiscount.php",
+						{ consumptionId: consumptionId, discountDescription:discountDescription ,discount: discount},
+						function(data, status){
+							table =$('.tableSelected');
+							selectTable(table);
+					});
+			}).keyup(function(e){
+			    if(e.keyCode == 13)
+			    {
+			        $(this).trigger("focusout");
+			    }
 			});
 		});
+		
+	});
+
 }
 function loadProducts(data, status){
 				var jsonData = JSON.parse(data);
@@ -332,19 +354,7 @@ function loadProducts(data, status){
 					addItemByProduct(product, table);									
 				});
 			}
-function loadAllCredits(){
-	$.post("Api/Consumption/GetAllCredits.php",
-			function(data, status){
-		$("#creditSelector").empty();
-		$('#creditSelector').append('<option value=""> Seleccione una cuenta ...</option>');
-		credits = JSON.parse(data);
-		if (credits.length == 0) { return; };
-		
-		$.each(credits, function(index, credit){					
-			$('#creditSelector').append('<option value='+credit.id+'>'+credit.description+'</option>');
-		});
-	});
-}
+
 function loadOpenConsumptionTables(){
 	$.post("Api/Consumption/GetAllOpenConsumptions.php",
 			function(data, status){
