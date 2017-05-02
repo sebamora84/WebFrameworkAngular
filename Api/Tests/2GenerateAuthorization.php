6cb212121896e0823ce25e3d4588ae4bbcc2066c
sebamora84 <sebamora@gmail.com>
2017-05-02 20:39:00 -0300

    fix authorization issues

--- Api/Tests/2GenerateAuthorization.php
+++ Api/Tests/2GenerateAuthorization.php
@@ -4,14 +4,13 @@ include_once '../Models/UserManager.php';
 
 $um = new UserManager();
 
-$index = $um->createResource(10,"index.html","Inicio",false);
-
+$index = $um->createResource(1,"","Inicio",false);
 
 $reports = $um->createResource(15,"reports.html","Reportes",true);
 $users = $um->createResource(16,"users.html","Usuarios",true);
 $system = $um->createResource(17,"system.html","Sistema",true);
 
-$root = $um->createResource(20,"","Root",false);
+
 $profile = $um->createResource(21,"profile.html","Perfil",false);
 $login = $um->createResource(22,"login.html","Iniciar Sesion",false);
 $notAuthorized= $um->createResource(23,"notAuthorized.html","No Autorizado",false);
@@ -22,7 +21,6 @@ $operario = $um->createRole("Operario");
 $usuario = $um->createRole("Usuario");
 
 $um->createRoleResource($usuario, $index);
-$um->createRoleResource($usuario, $root);
 $um->createRoleResource($usuario, $profile);
 
 $um->createRoleResource($administrador, $reports);
@@ -33,6 +31,6 @@ $um->createRoleResource($operario, $reports);
 
 $superuser = $um->getUserByName("superuser");
 $um->createUserRole($superuser->id, $usuario);
-$um->deleteUserRole($superuser,$usuario);
+$um->deleteUserRole($superuser->id,$usuario);
 
 ?>
