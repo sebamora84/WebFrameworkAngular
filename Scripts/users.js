b9d86660eec2e11a9600abc65fb74375534a4c96
sebamora84 <sebamora@gmail.com>
2017-05-14 01:15:08 -0300

    migrate users screen to angular

--- Scripts/users.js
+++ Scripts/users.js
@@ -1,88 +1,306 @@
 var app = angular.module('usersApp', []);
+
+//Controller for users 
 app.controller('usersCtrl', 
 	function($scope,$rootScope, $http) {
-		$http.post("Api/User/GetAllUsers.php")
-	    .then(function (response) {
-	    	$scope.users = response.data;
-	    	});
-		$scope.selectedUser = function(user) {
-				$scope.$emit('userSelected', user);
+		//Link actions		
+		$scope.selectedUserChanged = selectedUserChanged;
+		$scope.generateNewUser = generateNewUser;
+		$scope.resetUser = resetUser;
+		$scope.deleteUser = deleteUser;
+	    //Event listeners
+				
+	    //Functions
+		function loadAllUsers(){
+	    	$http.post("Api/User/GetAllUsers.php")
+	        .then(function (response) {
+	        	$scope.users = response.data;
+	        	});	
+	    }
+	    function selectedUserChanged(user) {
+			$scope.selectedUser= user;
+			$scope.$emit('selectedUserChanged', $scope.selectedUser);
 			};
+				
+		function generateNewUser(){
+			var newUserName = $scope.newUserName;
+			$scope.newUserName="";
+			$http({
+				 url: 'Api/User/SaveUser.php',
+			     method: 'POST',
+			     data: 'username='+newUserName,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAllUsers();
+			});				
+		};
+		function resetUser(){
+			$http({
+				 url: 'Api/User/ResetPassword.php',
+			     method: 'POST',
+			     data: 'username='+$scope.selectedUser.username,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAllUsers();
+			});				
+		};
+		function deleteUser(){
+			$http({
+				 url: 'Api/User/DeleteUser.php',
+			     method: 'POST',
+			     data: 'username='+$scope.selectedUser.username,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAllUsers();
+			});				
+		};
+
+		//Initializations
+		loadAllUsers();
 });
 
-app.controller('availableRolesCtrl', 
-	function($scope, $rootScope, $http) {
-		$http.post("Api/User/GetAllRoles.php")
-	    .then(function (response) {
-	    	$scope.availableRoles = response.data;
-	    	});
-});
 
+//Controlelr for assigned roles 
 app.controller('assignedRolesCtrl', 
 		function($scope, $rootScope, $http) {
-		$scope.assignedRoles=null;
-		$rootScope.$on('userSelected', function(event, user) {			
-		
+		//Link actions
+	    $scope.unassignRole = unassignRole;
+	    
+	    //Event listeners
+		$rootScope.$on('selectedUserChanged', function(event, selectedUser){
+			$scope.selectedUser = selectedUser;
+			loadAssignedRoles();
+			});
+		$rootScope.$on('roleAssigned', function(event, role){
+			loadAssignedRoles();
+			});
+		$rootScope.$on('rolesLoaded', function(event, roles){
+			loadAssignedRoles();
+			});
+		//Functions
+		function unassignRole(role){
+			//Check there's a selected user
+			if($scope.selectedUser==null){
+				$scope.assignedRoles==null;
+				return;
+			}
+			
+			$http({
+				 url: 'Api/User/UnassignRole.php',
+			     method: 'POST',
+			     data: 'username='+$scope.selectedUser.username+'&roleId='+role.id,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAssignedRoles();
+			});
+		}	
+		function loadAssignedRoles() {
+			//Check there's a selected user
+			if($scope.selectedUser==null){
+				$scope.assignedRoles==null;
+				return;
+			}
+			
+			//load the roles
 			$http({
 			 url: 'Api/User/GetUserRoles.php',
 		     method: 'POST',
-		     data: "username="+user.username,
+		     data: "username="+$scope.selectedUser.username,
 		     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
-		})			
-	    .then(function (response) {
-	    	$scope.assignedRoles = response.data;
-	    	});
-		});				
+			})			
+		    .then(function (response) {
+		    	$scope.assignedRoles = response.data;
+		    	});
+		};	
+	});
+
+//Controller for available roles 
+app.controller('availableRolesCtrl', 
+	function($scope, $rootScope, $http) {
+		//Link actions
+		$scope.assignRole = assignRole;
+		
+		//Event Listeners
+		$rootScope.$on('selectedUserChanged', function(event, selectedUser){
+			$scope.selectedUser = selectedUser;
+			});
+		
+		$rootScope.$on('rolesLoaded', function(event, roles){
+			$scope.availableRoles = roles;
+			});
+		//Functions
+		function assignRole(role){
+			//Check there's a selected user
+			if($scope.selectedUser==null){
+				return;
+			}
+			
+			$http({
+				 url: 'Api/User/AssignRole.php',
+			     method: 'POST',
+			     data: 'username='+$scope.selectedUser.username+'&roleId='+role.id,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   $scope.$emit('roleAssigned', role);
+			});
+		}
+});
+
+
+
+//Controller for roles 
+app.controller('rolesCtrl', 
+	function($scope, $rootScope, $http) {
+		//Link actions
+		$scope.selectedRoleChanged = selectedRoleChanged;
+		$scope.generateNewRole = generateNewRole;
+		$scope.deleteRole = deleteRole;
+		//Event listeners
+		
+		//Functions
+		function loadAllRoles(){
+	    	$http.post("Api/User/GetAllRoles.php")
+	        .then(function (response) {
+	        	$scope.roles = response.data;
+	        	$scope.$emit('rolesLoaded', $scope.roles);
+	        	});	
+	    }
+		function selectedRoleChanged(role) {
+			$scope.selectedRole= role;
+			$scope.$emit('selectedRoleChanged', $scope.selectedRole);
+			};
+		function generateNewRole(){
+			var newRoleDescription = $scope.newRoleDescription;
+			$scope.newRoleDescription="";
+			$http({
+				 url: 'Api/User/SaveRole.php',
+			     method: 'POST',
+			     data: 'roleDescription='+newRoleDescription,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAllRoles();
+			});				
+		};
+		function deleteRole(){
+			$http({
+				 url: 'Api/User/DeleteRole.php',
+			     method: 'POST',
+			     data: 'roleId='+$scope.selectedRole.id,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAllRoles();
+			});				
+		};
+		//Initialization
+		loadAllRoles();		
+});
+
+
+
+//Controlelr for assigned resources 
+app.controller('assignedResourcesCtrl', 
+		function($scope, $rootScope, $http) {
+		//Link actions
+	    $scope.unassignResource = unassignResource;
+	    
+	    //Event listeners
+		$rootScope.$on('selectedRoleChanged', function(event, selectedRole){
+			$scope.selectedRole = selectedRole;
+			loadAssignedResources();
+			});
+		$rootScope.$on('resourceAssigned', function(event, resource){
+			loadAssignedResources();
+			});
+		//Functions
+		function unassignResource(resource){
+			//Check there's a selected user
+			if($scope.selectedRole==null){
+				$scope.assignedResources==null;
+				return;
+			}
+			
+			$http({
+				 url: 'Api/User/UnassignResource.php',
+			     method: 'POST',
+			     data: 'roleId='+$scope.selectedRole.id+'&resourceId='+resource.id,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   loadAssignedResources();
+			});
+		}	
+		function loadAssignedResources() {
+			//Check there's a selected role
+			if($scope.selectedRole==null){
+				$scope.assignedResources==null;
+				return;
+			}
+			
+			//load the resources
+			$http({
+			 url: 'Api/User/GetRoleResources.php',
+		     method: 'POST',
+		     data: "roleId="+$scope.selectedRole.id,
+		     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		    .then(function (response) {
+		    	$scope.assignedResources = response.data;
+		    	});
+		};	
 	});
 
 
-var users;
-var selectedUser;
-var allRoles;
-var userRoles;
-var selectedRole;
-var allResources;
-var roleResources;
+//Controller for available roles 
+app.controller('availableResourcesCtrl', 
+	function($scope, $rootScope, $http) {
+		//Link actions
+		$scope.assignResource = assignResource;
+		
+		//Event Listeners
+		$rootScope.$on('selectedRoleChanged', function(event, selectedRole){
+			$scope.selectedRole = selectedRole;
+			});
+		
+		//Functions
+		function loadAllResources(){
+	    	$http.post("Api/User/GetAllResources.php")
+	        .then(function (response) {
+	        	$scope.availableResources = response.data;	        	
+	        	});	
+	    }
+		function assignResource(resource){
+			//Check there's a selected role
+			if($scope.selectedRole==null){
+				return;
+			}
+			
+			$http({
+				 url: 'Api/User/AssignResource.php',
+			     method: 'POST',
+			     data: 'roleId='+$scope.selectedRole.id+'&resourceId='+resource.id,
+			     headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},	
+			})			
+		   .then(function (response) {
+			   $scope.$emit('resourceAssigned', resource);
+			});
+		}
+		//Initialization
+		loadAllResources();
+});
+
 $(function() {
 		return;
-		loadAllResources();
-		loadAllRoles();
-		loadAllUsers();
 		
-		$('#resetUserButton').click(function(){
-			username = selectedUser.username;
-			if (username==""){
-				return;
-			}
-			$.post("Api/User/ResetPassword.php",
-					{username:username },
-					function(data, status){
-						loadAllUsers();
-				});
-		});
-		$('#saveUserButton').click(function(){
-			username = $("#userName").val();
-			if (username==""){
-				return;
-			}
-			$.post("Api/User/SaveUser.php",
-					{username:username },
-					function(data, status){
-						$('#userName').val("");
-						loadAllUsers();
-				});
-		});
-		$('#deleteUserButton').click(function(){
-			username = selectedUser.username;
-			if (username==""){
-				return;
-			}
-			$.post("Api/User/DeleteUser.php",
-					{username:username },
-					function(data, status){
-						loadAllUsers();
-				});
-		});
+		
+		
+		
 		$('#saveRoleButton').click(function(){
 			roleDescription = $("#roleDescription").val();
 			if (roleDescription==""){
@@ -109,38 +327,6 @@ $(function() {
 				});
 		});
 });
-function loadAllUsers(){
-	$.post("Api/User/GetAllUsers.php",
-			function(data, status){
-		$("#userTable tbody").empty();
-		users = JSON.parse(data);
-		if (users.length == 0) { return; };
-		
-		$.each(users, function(index, user){					
-			$("#userTable tbody").append(
-					'<tr id="user'+user.id+'">'+
-						'<td class="col1">'+
-							'<label id="userId'+user.id+'">'+user.id+'</label>'+
-						'</td>'+
-						'<td>'+
-							'<label id="username'+user.id+'" class="username">'+user.username+'</label>'+
-						'</td>'+
-						'<td>'+
-							'<label id="userReset'+user.id+'" class="userEmail">'+user.reset+'</label>'+
-						'</td>'+
-					'</tr>');			
-		});
-		//Load the selected user
-		$('#userTable tr').has('td').click(function(){
-			$('tr').removeClass('trSelected');
-			$(this).addClass('trSelected');
-							
-			userId = $(this).find('td label')[0].innerHTML;
-			    selectedUser = users[userId];
-			    loadUserRoles();
-		});
-	});
-}
 function loadAllRoles(){
 	$("#availableRolesContainer").empty();
 	$("#roleTable tbody").empty();
